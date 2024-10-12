12b19be36839a3767bd6d5f153c3e360e1d11c59
Kodziak <kodziak1416@gmail.com>
2024-10-12 21:09:33 +0100

    update page

--- src/content.jsx
+++ src/content.jsx
@@ -7,11 +7,9 @@ setTimeout(() => {
   const root = document.createElement("div")
   root.id = "wrapper"
   const twitterPost = document.querySelector('[data-testid="tweetText"]')
-  if (twitterPost && twitterPost.textContent.includes('decode.me')) {
+  if (twitterPost && twitterPost.textContent.includes('swifli-frontend.vercel.app')) {
     const url = twitterPost.querySelector('a').textContent
     const id = url.split('/').pop()
-
-    console.log({id})
 
     document.querySelector('[data-testid="tweetText"]').appendChild(root)
 
